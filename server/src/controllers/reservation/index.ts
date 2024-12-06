4c13e9a8da71817c31829686b68f862ce243cc0c
Egle Dirmeityte <dirmeityteegle@gmail.com>
2024-12-06 17:28:13 +0200

    Remove comments

--- server/src/controllers/reservation/index.ts
+++ server/src/controllers/reservation/index.ts
@@ -10,17 +10,3 @@ export default router({
   getReservationsByBookCopy,
   getReservationsByUser,
 })
-
-// changeReservationDate - check if doesn't clash with other reservation
-// getReservationsByBookCopy
-// getReservationsByOwner
-// getReservationsByReserver
-// what to do if end_date is today? Both reserver and owner get notifications, in frontend it's displayed that it's passed due date - either change end_date or complete it.
-
-// create
-// getReservationsByBookCopyId
-// getReservationsByOwnerId
-// getReservationsByReserverId
-// updateStatus
-// updateDates
-// Delete reservation
