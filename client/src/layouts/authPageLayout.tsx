f98225077ff5304d31c98ba4093f1cc8b4c6200f
Egle Dirmeityte <dirmeityteele@gmail.com>
2025-01-04 18:57:51 +0200

    feat: create initial layout

--- client/src/layouts/authPageLayout.tsx
+++ client/src/layouts/authPageLayout.tsx
@@ -2,8 +2,19 @@ import { Outlet } from "react-router-dom";
 
 export default function authPageLayout() {
   return (
-    <div>
-      <Outlet />
+    <div className="grid grid-cols-[min-content_auto] gap-2 min-h-screen">
+      <div className="h-full">Navigation</div>
+      <div className="h-full  w-full">
+        <header>
+          Header
+        </header>
+        <main className="h-full">
+        <Outlet />
+        </main>
+        <footer>
+          Footer
+        </footer>
+      </div>
     </div>
   )
 }
--- client/src/pages/dashboardPage.tsx
+++ client/src/pages/dashboardPage.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/button";
-
 export default function dashboard() {
   return (
     <div>
-      <h1 className="text-2xl">Dashboard</h1>
-      <Button>Click me</Button>
+      <h1>Dashboard</h1>
     </div>
   )
 }
