2948fb37a97836e9bc658109a106f18b15806d86
Egle Dirmeityte <dirmeityteele@gmail.com>
2025-01-05 14:24:10 +0200

    feat: add screen brakpoints for main container

--- client/src/layouts/authPageLayout.tsx
+++ client/src/layouts/authPageLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function authPageLayout() {
   return (
-    <div className="grid grid-cols-[min-content_auto] gap-2 min-h-screen">
+    <div className="grid grid-cols-[min-content_auto] gap-2 min-h-screen 2xl:max-w-[60%] mx-auto">
       <div className="h-full">Navigation</div>
       <div className="h-full  w-full">
         <header>
